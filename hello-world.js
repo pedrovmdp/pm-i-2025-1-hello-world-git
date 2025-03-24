e633d7cb373ea44e23fd5d4a696355358cc1b9bc
Pedro Vinícius <pedromichelwork@gmail.com>
2025-03-24 20:26:22 -0300

    Fazendo Alteracoes professor

--- hello-world.js
+++ hello-world.js
@@ -5,8 +5,32 @@ nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
         messageP.innerText = '';
     }
+
+
+if (event.key === 'Enter') {
+    helloWorld();
+    }
+if (event.key === 'Escape') {
+    nameInput.value = '';
+    messageP.innerText = '';
+    }
+
 }
 
+
 const helloWorld = () => {
-    messageP.innerText = `Hello World, ${nameInput.value}!`;
+    const name = nameInput.value.trim();
+    if (name) {
+    messageP.innerText = `Hello World, ${name}!`;
+    } else {
+        messageP.innerText = `Hello World!`;
+    }
+
+
 }
+
+const clearButton = () => {
+    nameInput.value = '';
+    messageP.innerText = '';
+};
+
